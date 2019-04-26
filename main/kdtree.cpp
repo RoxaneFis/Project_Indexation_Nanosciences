5c3ea940085ed52e1651f99ca5f2307565dc9479
alecdavi <alecleodavi@gmail.com>
2019-04-26 15:34:51 +0200

    kdtree avec median

--- main/kdtree.cpp
+++ main/kdtree.cpp
@@ -1,2 +1,11 @@
 #include "kdtree.h"
 
+
+kdtree::computeMedian(point* P,int start, int end, int axe){
+
+        double coords [end-start];
+        for (int i=start; i<end; i++)
+          coords[i-start] = P[i].getcoord(axe);
+        std::sort (coords, coords + end-start);
+        return coords [(end-start)/2];
+ }
