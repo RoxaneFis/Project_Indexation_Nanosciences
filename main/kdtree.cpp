6606e3c0ccc020a0d7e9e77693233c06d2379ea3
Fischer <roxane.fischer@polytechnique.edu>
2019-04-26 14:15:28 +0200

    Kdtree : chgmt structure

--- main/kdtree.cpp
+++ main/kdtree.cpp
@@ -1,29 +1,2 @@
 #include "kdtree.h"
 
-class kdtreeData : public QSharedData
-{
-public:
-
-};
-
-kdtree::kdtree() : data(new kdtreeData)
-{
-
-}
-
-kdtree::kdtree(const kdtree &rhs) : data(rhs.data)
-{
-
-}
-
-kdtree &kdtree::operator=(const kdtree &rhs)
-{
-    if (this != &rhs)
-        data.operator=(rhs.data);
-    return *this;
-}
-
-kdtree::~kdtree()
-{
-
-}
--- main/kdtree.h
+++ main/kdtree.h
@@ -1,25 +1,15 @@
-#ifndef KDTREE_H
+
 #define KDTREE_H
-
-#include <QDeclarativeItem>
-#include <QMainWindow>
-#include <QObject>
-#include <QQuickItem>
-#include <QSharedDataPointer>
-#include <QWidget>
-
-class kdtreeData;
+#include <iostream>
 
 class kdtree
 {
 public:
     kdtree();
-    kdtree(const kdtree &);
-    kdtree &operator=(const kdtree &);
+
     ~kdtree();
 
 private:
-    QSharedDataPointer<kdtreeData> data;
+
 };
 
-#endif // KDTREE_H
