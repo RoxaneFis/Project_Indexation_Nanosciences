ca635d517bc0f57fa1ada95e4ac51eaa8ef31188
alecdavi <alecleodavi@gmail.com>
2019-04-26 14:49:03 +0200

    Point

--- main/point.h
+++ main/point.h
@@ -1,11 +1,20 @@
 #ifndef POINT_H
 #define POINT_H
+#include <iostream>
+#include <string>
 
 
 class Point
 {
 public:
     Point();
+    ~Point();
+
+    static int d;
+    double* coords;
+    std::string label;
+
+
 };
 
 #endif // POINT_H
