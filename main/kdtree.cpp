5b1b7f1d077c582e85a312be253ad5880e139eb4
alecdavi <alecleodavi@gmail.com>
2019-05-10 14:55:45 +0200

    Debut search

--- main/kdtree.cpp
+++ main/kdtree.cpp
@@ -1,7 +1,5 @@
 #include "kdtree.h"
 #include "point.h"
-#include <list>
-
 
 
 //noeud(Point _p);
@@ -20,6 +18,7 @@ noeud* kdtree::build (Point* P, int start, int end, int c, int dim) {
 
   else {  // internal node
       for (int i=start; i<end; i++){
+        //print(P[i],dim);
        std::cout << P[i].getcoord(0) << ((i==end-1)?"\n":" ");
       }
     }
@@ -37,6 +36,11 @@ noeud* kdtree::build (Point* P, int start, int end, int c, int dim) {
 kdtree::kdtree(Point* P, int start, int end, int c, int dim){
     racine = build (P, start, end, c,  dim);
 }
+
+
+
+
+
 
 
 void kdtree::swap(Point* P, int i, int j) {
@@ -72,8 +76,57 @@ int kdtree::partition(Point* P, int start, int end, int axe, int dim) {
   return p;
 }
 
+std::list<result> kdtree::search( Point q, float r){
+
+    std::list<result> reponse();
+    std::list<noeud> attente();
+
+    attente.push_front(racine);
+
+    while (attente.size()>0) {
+        noeud* a= attente.pop_front();
 
 
 
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+int main(){
+
+
+      int n = 5;  // n points in R^{dim}
+      int dim = 2;
+      // random input data points (uniformly sampled in unit cube)
+      srand (time(NULL));
+      Point * P[n];
+      for (int i=0; i<n; i++) {
+          double *c;
+          c={1,2};
+          P[i] =Point(c, "t");
+        for (int j=0; j<dim; j++)
+          P[i][j] = i;
+      }
+      noeud* f = build ( P, 0, n, 0,  dim);
+      //std::cout<<"debut built" << f->m << f->left->m <<f->right->m <<"fin built"<< std::endl;
+
+
+
+
+
+    return 0;
+}
 
 
